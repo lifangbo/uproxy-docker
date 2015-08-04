45821a41992a2a336281a4b99d76c2c60d4c2597
Lally Singh <lally@google.com>
2015-08-04 11:12:14 -0400

    Fix docker-name test.

--- testing/run-scripts/run_pair.sh
+++ testing/run-scripts/run_pair.sh
@@ -62,7 +62,8 @@ else
 fi
 
 function make_image () {
-    if [ $(docker images | tail -n +2 | awk '{print $1}' | /bin/grep uproxy/$1) == "uproxy/$1" ]
+    local CANDIDATES=$(docker images | tail -n +2 | awk '{print $1}' | /bin/grep uproxy/$1)
+    if [ "$CANDIDATES" == "uproxy/$1" ]
     then
         echo "Reusing existing image uproxy/$1"
     else
